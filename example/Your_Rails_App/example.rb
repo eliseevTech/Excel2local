8d995fc460c7436f0e7c1243e8c9e6eeea695b0e
EliseevEV <Eliseevmail@ya.ru>
2018-09-23 23:07:10 +0300

    New version
    New function for localize all excel file in the folder.

--- example/Your_Rails_App/example.rb
+++ example/Your_Rails_App/example.rb
@@ -4,6 +4,8 @@
 # gem "excel2local", '~> 0.1.3' to 
 require "Excel2local" 
 
-# add Excel2local::localize! './config/locales/locales.xlsx','./config/locales' to config.ru
-Excel2local::localize! './config/locales/locales.xlsx','./config/locales'
+# add next for localize only one file './config/locales/locales.xlsx' to './config/locales'
+#Excel2local::localize! './config/locales/locales.xlsx','./config/locales'
 
+#add next to config.ru for localize all files from './config/locales'
+Excel2local::localize_all! './config/locales'
--- lib/excel2local.rb
+++ lib/excel2local.rb
@@ -2,6 +2,13 @@ require "excel2local/version"
 require "roo"
 
 module Excel2local
+  
+  def self.localize_all! (locate)
+    Dir["#{locate}/*.xlsx"].each { |file| 
+      localize! "#{file}","#{locate}"
+    }
+  end
+
   def self.localize! (excel_file, save_place)    #excel_file - фаил с переводом 
     puts "Excel2local::localize! from #{excel_file} ---> to #{save_place}/"
     workbook = Roo::Excelx.new(excel_file)    #подсовываем фаил в библиотеку
@@ -10,7 +17,7 @@ module Excel2local
     workbook.each_with_index {|row, row_index|    #начинаем перебор строк
       if (row_index == 0) then    #если первая строка то начинаем создавать файлы локализаций
         row.each_with_index { |col, col_index|    # перебираем колонки
-          File.open("#{save_place}/#{col}.yml", "w") { |file| file.puts "#{col}:"   } if col_index != 0 && col != "#" #если не 1 колонка то создаем фаил локали 
+          File.open("#{save_place}/#{File.basename(excel_file, ".xlsx")}_#{col}.yml", "w") { |file| file.puts "#{col}:"   } if col_index != 0 && col != "#" #если не 1 колонка то создаем фаил локали 
           yml_file[col_index] = col    #кладем имя файла в массив в соответствующий индекс
           simbol_memory[col_index] = []
         } 
@@ -22,13 +29,13 @@ module Excel2local
             @simbol.to_s.split(".").each_with_index { | simbol, index |
               simbol = "\'" + simbol + "\'" if [ "true", "false", "on", "off", "yes", "no" ].member?(simbol)
               if simbol != simbol_memory[col_index][index]  then
-                File.open("#{save_place}/#{yml_file[col_index]}.yml", "a") { |file| file.print "  "*(index+1) + "#{simbol}:"   }  if @simbol.to_s.split(".").length - 1 == index
-                File.open("#{save_place}/#{yml_file[col_index]}.yml", "a") { |file| file.puts "  "*(index+1) + "#{simbol}:"  }  if @simbol.to_s.split(".").length - 1 != index
+                File.open("#{save_place}/#{File.basename(excel_file, ".xlsx")}_#{yml_file[col_index]}.yml", "a") { |file| file.print "  "*(index+1) + "#{simbol}:"   }  if @simbol.to_s.split(".").length - 1 == index
+                File.open("#{save_place}/#{File.basename(excel_file, ".xlsx")}_#{yml_file[col_index]}.yml", "a") { |file| file.puts "  "*(index+1) + "#{simbol}:"  }  if @simbol.to_s.split(".").length - 1 != index
                 index.upto(@simbol.to_s.split(".").length ) { |n| simbol_memory[col_index].delete_at(n)}
               end
               simbol_memory[col_index][index] = simbol 
             }
-            File.open("#{save_place}/#{yml_file[col_index]}.yml", "a") { |file| file.puts "  \'#{col}\'"  }  # пишем сам символ
+            File.open("#{save_place}/#{File.basename(excel_file, ".xlsx")}_#{yml_file[col_index]}.yml", "a") { |file| file.puts "  \'#{col}\'"  }  # пишем сам символ
           end
         }
       end
